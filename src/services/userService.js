42783d664836424c07c2d7dd76ffe908f6f390ce
Agussit <agustin.tammone@hotmail.com>
2024-04-22 22:29:37 -0300

    fix:valizacion completa

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,11 @@
 const { validationResult } = require('express-validator');
 const userService = {
-    validateOne: (body, res) => {
-        const resultValidation = validationResult(body);
+    validateOne: (fromUser) => {
+        const resultValidation = validationResult(fromUser);
         if (resultValidation.errors.length > 0) {
             return  {
-                errors: resultValidation.mapped()
+                errors: resultValidation.mapped(),
+                oldData: fromUser.body
             }
         }
 
