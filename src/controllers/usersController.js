bd4390f5f8095c1a4c790753e8cedfcc7695ce69
rodrigozaldivar <rodrigo@teracom.mx>
2024-04-23 18:58:16 -0600

    redirigir a home despues del registro

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,19 +1,22 @@
 const userService = require("../services/userService");
 
 const usersController = {
-	login : function(req,res){
-		res.render('login')
+	login: function (req, res) {
+		res.render("login");
 	},
 
-	register : function (req,res){
-		res.render('register')
+	register: function (req, res) {
+		res.render("register");
 	},
-	processRegister:function (req,res){
-		const validUser =userService.validateOne(req);
-		if (validUser.errors){
-        return res.render('register', validUser)}
-		return res.redirect('/')	
-	}
-}
+
+	processRegister: function (req, res) {
+		const validUser = userService.validateOne(req);
+		if (validUser.errors) {
+			return res.render("register", validUser);
+		} else {
+			return res.redirect("/");
+		}
+	},
+};
 
 module.exports = usersController;
