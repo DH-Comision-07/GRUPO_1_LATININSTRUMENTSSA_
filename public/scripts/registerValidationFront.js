3d0a842a6a65f2fc4dc566caf53dd70c547e526a
RodrigoIvanGonzalez <r.ivann.gonzalezz@gmail.com>
2024-06-09 15:44:49 -0300

    feature: creacion archivop validation front para register

---
+++ public/scripts/registerValidationFront.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function(){
+
+    let form = document.querySelector('.form');
+
+    form.addEventListener('form .form')
+
+    let fieldName= document.querySelector('input .name');
+    let fieldEmail= document.querySelector('input .email');
+    let fieldPass= document.querySelector('input .pass');
+    let fieldRepass= document.querySelector('input .repass');
+
+})
