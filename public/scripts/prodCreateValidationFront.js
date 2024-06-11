378568329ec1c0e0ec41010de8edb5233db55cd3
Agussit <agustin.tammone@hotmail.com>
2024-06-11 00:49:13 -0300

    fix:validando-product-create

---
+++ public/scripts/prodCreateValidationFront.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    let formulario = document.querySelector('.form');
+
+    formulario.addEventListener("submit", function (e) {
+
+        let fails = [];
+
+        let fieldName = document.querySelector('input.name');
+
+        if (fieldName.value == "") {
+            fails.push("Se requiere un nombre")
+        };
+
+        let fieldBrand = document.querySelector('input.brand');
+
+        if (fieldBrand.value == "") {
+            fails.push("Se requiere el nombre de la marca")
+        };
+
+        let fieldDesc = document.querySelector('input.description');
+
+        if (fieldDesc.value == "") {
+            fails.push("Se requiere una descripción")
+        };
+
+        let fieldCat = document.querySelector('select.category');
+
+        if (fieldCat.value == "") {
+            fails.push("Elija una categoría")
+        };
+
+        let fieldPrice = document.querySelector('input.price');
+
+        if (fieldPrice.value == "") {
+            fails.push("Elija una categoría")
+        };
+        
+        if (fails.length > 0) {
+
+            e.preventDefault();
+
+            let ulFails = document.querySelector("div.fails ul");
+
+            ulFails.innerHTML = "";
+
+            for (let i = 0; i < fails.length; i++) {
+                ulFails.innerHTML += "<li>" + fails[i] + "</li>"
+            }
+        }
+
+    })
+})
