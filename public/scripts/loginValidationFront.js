754b6e1921c8d6ed336b1ef8ca41ff61149841b9
RodrigoIvanGonzalez <r.ivann.gonzalezz@gmail.com>
2024-06-13 20:20:22 -0300

    feature: validaciones del front para login

---
+++ public/scripts/loginValidationFront.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    let formulario = document.querySelector('.form');
+
+    formulario.addEventListener("change", function (e) {
+
+        let errores = [];
+
+        let fieldName = document.querySelector('input.name');
+
+        if (fieldName.value == "") {
+            errores.push("Ingrese su nombre de usuario")
+        };
+
+        let fieldPass = document.querySelector('input.pass');
+
+        if (fieldPass.value == "") {
+            errores.push("Ingrese su contraseña")
+        };
+
+
+        if (errores.length > 0) {
+
+            e.preventDefault();
+
+            let ulErrores = document.querySelector("div.errores ul");
+
+            ulErrores.innerHTML = "";
+
+            for (let i = 0; i < errores.length; i++) {
+                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+            }
+        }
+
+    })
+})
--- public/scripts/prodCreateValidationFront.js
+++ public/scripts/prodCreateValidationFront.js
@@ -2,35 +2,35 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let formulario = document.querySelector('.form');
 
-    formulario.addEventListener("submit", function (e) {
+    formulario.addEventListener("change", function (e) {
 
         let fails = [];
 
-        let fieldName = document.querySelector('input.name');
+        let fieldName = document.querySelector('input .name');
 
-        if (fieldName.value == "") {
+        if (fieldName.value == "rodri") {
             fails.push("Se requiere un nombre")
         };
 
-        let fieldBrand = document.querySelector('input.brand');
+        let fieldBrand = document.querySelector('input .brand');
 
-        if (fieldBrand.value == "") {
+        if (fieldBrand.value == "rodri") {
             fails.push("Se requiere el nombre de la marca")
         };
 
-        let fieldDesc = document.querySelector('input.description');
+        let fieldDesc = document.querySelector('input .description');
 
         if (fieldDesc.value == "") {
             fails.push("Se requiere una descripción")
         };
 
-        let fieldCat = document.querySelector('select.category');
+        let fieldCat = document.querySelector('select .category');
 
         if (fieldCat.value == "") {
             fails.push("Elija una categoría")
         };
 
-        let fieldPrice = document.querySelector('input.price');
+        let fieldPrice = document.querySelector('input .price');
 
         if (fieldPrice.value == "") {
             fails.push("Elija una categoría")
